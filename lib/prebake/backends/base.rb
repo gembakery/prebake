4e08c0c77ed855e3a5f8bf3d478a1dacfeb1acb3
Thejus Paul <thejuspaul@pm.me>
2026-03-25 00:21:06 +0530

    refactor(backends): consolidate insecure HTTP warning and default base methods

--- lib/prebake/backends/base.rb
+++ lib/prebake/backends/base.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../logger"
+
 module Prebake
   module Backends
     class Base
@@ -7,24 +9,46 @@ module Prebake
         raise NotImplementedError, "#{self.class}#fetch not implemented"
       end
 
-      def fetch_checksum(cache_key)
-        raise NotImplementedError, "#{self.class}#fetch_checksum not implemented"
+      def fetch_checksum(_cache_key)
+        nil
       end
 
       def push(gem_path, cache_key, checksum)
         raise NotImplementedError, "#{self.class}#push not implemented"
       end
 
-      def exists?(cache_key)
-        raise NotImplementedError, "#{self.class}#exists? not implemented"
+      def exists?(_cache_key)
+        false
       end
 
-      def delete(cache_key)
-        raise NotImplementedError, "#{self.class}#delete not implemented"
+      def delete(_cache_key)
+        false
       end
 
       def checksums_supported?
         true
+      end
+
+      protected
+
+      def warn_if_insecure_http(url)
+        return if url.start_with?("https://")
+        return if ENV.fetch("PREBAKE_ALLOW_INSECURE", "false") == "true"
+        return unless url.start_with?("http://")
+        return if darwin_with_default_host?(url)
+
+        Logger.warn(
+          "Using insecure HTTP connection to #{url}. " \
+          "Set PREBAKE_ALLOW_INSECURE=true to suppress this warning."
+        )
+      end
+
+      private
+
+      def darwin_with_default_host?(url)
+        RUBY_PLATFORM.include?("darwin") &&
+          Prebake.backend_type == "http" &&
+          url.chomp("/") == Prebake::DEFAULT_HTTP_URL
       end
     end
   end
