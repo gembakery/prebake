dbf1e78560bac15558defc56a59a1dc98c28150f
Thejus Paul <thejuspaul@pm.me>
2026-03-31 09:29:58 +0530

    test: add edge case tests for ExtensionValidator

--- test/extension_validator_test.rb
+++ test/extension_validator_test.rb
@@ -81,4 +81,78 @@ class ExtensionValidatorTest < Minitest::Test
     assert File.exist?(dest), "Expected bar.so to be copied to extension_dir root"
     assert_equal "lib-binary", File.read(dest)
   end
+
+  def test_preserves_nested_lib_subdirectory
+    FileUtils.touch(File.join(@extension_dir, ".prebake"))
+
+    # lib/<subdir>/foo.so should NOT be copied (intentional nested layout)
+    nested_lib = File.join(@extension_dir, "lib", "subdir")
+    FileUtils.mkdir_p(nested_lib)
+    File.write(File.join(nested_lib, "foo.so"), "nested-lib-binary")
+
+    spec = mock("spec")
+    spec.stubs(:extension_dir).returns(@extension_dir)
+
+    Prebake::ExtensionValidator.validate(spec)
+
+    refute File.exist?(File.join(@extension_dir, "foo.so")),
+      "Should not copy binaries from lib/<subdir>/ (only direct children of lib/)"
+  end
+
+  def test_skips_symlinks
+    FileUtils.touch(File.join(@extension_dir, ".prebake"))
+
+    lib_dir = File.join(@extension_dir, "lib")
+    FileUtils.mkdir_p(lib_dir)
+
+    real_file = File.join(@tmpdir, "real.so")
+    File.write(real_file, "real-binary")
+    File.symlink(real_file, File.join(lib_dir, "linked.so"))
+
+    spec = mock("spec")
+    spec.stubs(:extension_dir).returns(@extension_dir)
+
+    Prebake::ExtensionValidator.validate(spec)
+
+    refute File.exist?(File.join(@extension_dir, "linked.so")),
+      "Should not copy symlinked binaries"
+  end
+
+  def test_skips_empty_files
+    FileUtils.touch(File.join(@extension_dir, ".prebake"))
+
+    lib_dir = File.join(@extension_dir, "lib")
+    FileUtils.mkdir_p(lib_dir)
+    FileUtils.touch(File.join(lib_dir, "empty.so"))
+
+    spec = mock("spec")
+    spec.stubs(:extension_dir).returns(@extension_dir)
+
+    Prebake::ExtensionValidator.validate(spec)
+
+    refute File.exist?(File.join(@extension_dir, "empty.so")),
+      "Should not copy zero-byte binaries"
+  end
+
+  def test_idempotent
+    FileUtils.touch(File.join(@extension_dir, ".prebake"))
+
+    nested_dir = File.join(@extension_dir, "extension", "x86_64-linux", "4.0.0")
+    FileUtils.mkdir_p(nested_dir)
+    File.write(File.join(nested_dir, "foo.so"), "binary-content")
+
+    spec = mock("spec")
+    spec.stubs(:extension_dir).returns(@extension_dir)
+
+    Prebake::ExtensionValidator.validate(spec)
+    Prebake::ExtensionValidator.validate(spec)
+
+    dest = File.join(@extension_dir, "foo.so")
+    assert File.exist?(dest), "Expected foo.so to exist after two runs"
+    assert_equal "binary-content", File.read(dest)
+
+    # Second run hits fast path (root binary exists), no duplicates
+    root_binaries = Dir.glob(File.join(@extension_dir, "*.so"))
+    assert_equal 1, root_binaries.length, "Should not duplicate files on second run"
+  end
 end
